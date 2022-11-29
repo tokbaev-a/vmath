d5e28e519abf37bf8e3578abde533319642a1cbe
Arsen Tokbaev <tokbaev_a@auca.kg>
2022-11-29 12:48:36 +0100

    Create Vec3 skeleton

---
+++ lib/src/main/java/vmath/Vec3.java
@@ -0,0 +1,53 @@
+package vmath;
+
+public class Vec3 {
+    private float x, y, z;
+
+    public Vec3() {
+        this(0.0f, 0.0f, 0.0f);
+    }
+
+    public Vec3(float x, float y, float z) {
+        // TODO
+    }
+
+    public float x() {
+        return 0.0f; // TODO
+    }
+
+    public float y() {
+        return 0.0f; // TODO
+    }
+
+    public float z() {
+        return 0.0f; // TODO
+    }
+
+    public float mag() {
+        return 0.0f; // TODO
+    }
+
+    public Vec3 norm() {
+        return null; // TODO
+    }
+
+    public Vec3 add(Vec3 other) {
+        return null; // TODO
+    }
+
+    public Vec3 sub(Vec3 other) {
+        return null; // TODO
+    }
+
+    public Vec3 mul(float scalar) {
+        return null; // TODO
+    }
+
+    public float dot(Vec3 other) {
+        return 0.0f; // TODO
+    }
+
+    public String toString() {
+        return ""; // TODO
+    }
+}
