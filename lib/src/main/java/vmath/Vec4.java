3794fd6d7d902fb8bcb28c206fae3d652205c432
Arsen Tokbaev <tokbaev_a@auca.kg>
2022-11-29 15:07:54 +0100

    Create Vec4 skeleton

---
+++ lib/src/main/java/vmath/Vec4.java
@@ -0,0 +1,61 @@
+package vmath;
+
+public class Vec4 {
+    private final float x, y, z, w;
+
+    public Vec4() {
+        this(0.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    public Vec4(float x, float y, float z, float w) {
+        // TODO
+        this.x = 0.0f;
+        this.y = 0.0f;
+        this.z = 0.0f;
+        this.w = 0.0f;
+    }
+
+    public float x() {
+        return 0.0f; // TODO
+    }
+
+    public float y() {
+        return 0.0f; // TODO
+    }
+
+    public float z() {
+        return 0.0f; // TODO
+    }
+
+    public float w() {
+        return 0.0f; // TODO
+    }
+
+    public float mag() {
+        return 0.0f; // TODO
+    }
+
+    public Vec4 norm() {
+        return null; // TODO
+    }
+
+    public Vec4 add(Vec4 other) {
+        return null; // TODO
+    }
+
+    public Vec4 sub(Vec4 other) {
+        return null; // TODO
+    }
+
+    public Vec4 mul(float scalar) {
+        return null; // TODO
+    }
+
+    public float dot(Vec4 other) {
+        return 0.0f; // TODO
+    }
+
+    public String toString() {
+        return ""; // TODO
+    }
+}
